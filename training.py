54666ce28cb0152c7da63f2bbba2eac2b785ce83
Muddasar Khan <58143077+webnseo365days@users.noreply.github.com>
2023-12-14 18:00:02 +0500

    Update training.py

--- training.py
+++ training.py
@@ -7,13 +7,29 @@ import numpy as np       # numpy is very famous for handling arrays / matrices
 # add pictures in each directory
 people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 
-p =[]
+
+ 
+p = []  # Create an empty list named 'p'
+# Iterate over the contents of the directory "Faces\train"
 for i in os.listdir(r"Faces\train"):
-    p.append(i)
+    p.append(i)  # Append each item (file or directory name) to the list 'p'
+# The r before the string (e.g., r"Faces\train") denotes a raw string literal, which is used to interpret backslashes as literal characters rather than escape characters. 
+# This is often used with file paths to avoid issues with backslashes being interpreted as escape characters.
+# p.append(i): Inside the loop, each item (i) is appended to the list p. 
+# This means that after the loop completes, p will contain all the file or directory names present in the "Faces\train" directory.
 
 DIR = r"Faces\train"
 
 haar_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# This line of code is using OpenCV (cv2) to create a Haar Cascade Classifier for detecting frontal faces in images. Let's break it down:
+# cv.data.haarcascades: This is a path to the directory containing Haar Cascade XML files provided by OpenCV. 
+# These XML files define the features and parameters for object detection using Haar cascades.
+# 'haarcascade_frontalface_default.xml': This is the specific Haar Cascade XML file for detecting frontal faces. 
+# The file contains information about the patterns and features that constitute a frontal face.
+# cv.CascadeClassifier(...): This part initializes a CascadeClassifier object, which is a classifier based on Haar cascades. 
+# It takes the complete path to the XML file as an argument.
+
+
 
 faces = []
 labels = []
